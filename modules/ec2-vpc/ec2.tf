fd6b66c4c01b90c537f54bf1b8df4f0e651e17a1
sasatake <t06418as@gmail.com>
2020-10-10 07:49:11 +0900

    add ec2 modules

---
+++ modules/ec2-vpc/ec2.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazonlinux2.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name            = "${var.prefix}-ec2-instance"
+    ProvisionedBy   = "Terraform"
+    ProvisionedFrom = "local"
+  }
+}
+
+data "aws_ami" "amazonlinux2" {
+  owners      = ["amazon"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["Amazon Linux 2*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
---
+++ modules/ec2-vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  type    = string
+  default = "test"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t4g.micro"
+}
