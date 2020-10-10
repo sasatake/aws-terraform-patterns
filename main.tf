2bed1314409e52ad57c2c050e6eea53cdb9265db
sasatake <t06418as@gmail.com>
2020-10-11 07:01:11 +0900

    add ec2-vpc module to main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,9 @@ provider "aws" {
   version = "~> 3"
   region  = var.main_region
 }
+
+module "ec2-vpc" {
+  source = "./modules/ec2-vpc/"
+
+  prefix = var.resource_prefix
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "main_region" {
   type    = string
   default = "ap-northeast-1"
 }
+
+variable "resource_prefix" {
+  type    = string
+  default = "sasatake-test"
+}
