4e9b2387723743ca4d221151ea3e7b241c6ade35
sasatake <t06418as@gmail.com>
2020-11-24 17:29:27 +0900

    add ecs service task size controller document

--- modules/ecs-ssm/ssm.tf
+++ modules/ecs-ssm/ssm.tf
@@ -7,3 +7,19 @@ resource "aws_ssm_parameter" "ecssrv_task_size" {
     ignore_changes = [value]
   }
 }
+
+resource "aws_ssm_document" "ecssrv-scale-in" {
+  name            = "${var.prefix}-ssmdoc-scale-in"
+  document_type   = "Automation"
+  document_format = "YAML"
+
+  content = file("${path.module}/templates/ssm/document/scale-in.yaml")
+}
+
+resource "aws_ssm_document" "ecssrv-scale-out" {
+  name            = "${var.prefix}-ssmdoc-scale-out"
+  document_type   = "Automation"
+  document_format = "YAML"
+
+  content = file("${path.module}/templates/ssm/document/scale-out.yaml")
+}
