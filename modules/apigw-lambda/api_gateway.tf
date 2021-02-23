5675d05b09df9ccb5d1a79a70f8ae0bc967548fa
sasatake <t06418as@gmail.com>
2021-02-23 18:02:11 +0900

    add cors method

--- modules/apigw-lambda/api_gateway.tf
+++ modules/apigw-lambda/api_gateway.tf
@@ -9,20 +9,61 @@ resource "aws_api_gateway_resource" "serverless_api" {
   path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_method" "serverless_api" {
+resource "aws_api_gateway_method" "serverless_api_lambda" {
   rest_api_id   = aws_api_gateway_rest_api.serverless_api.id
   resource_id   = aws_api_gateway_resource.serverless_api.id
   http_method   = "ANY"
   authorization = "AWS_IAM"
 }
 
-resource "aws_api_gateway_integration" "serverless_api" {
+resource "aws_api_gateway_integration" "serverless_api_lambda" {
   rest_api_id             = aws_api_gateway_rest_api.serverless_api.id
   resource_id             = aws_api_gateway_resource.serverless_api.id
-  http_method             = aws_api_gateway_method.serverless_api.http_method
+  http_method             = aws_api_gateway_method.serverless_api_lambda.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.serverless_api.invoke_arn
+}
+
+resource "aws_api_gateway_method" "serverless_api_cors" {
+  rest_api_id   = aws_api_gateway_rest_api.serverless_api.id
+  resource_id   = aws_api_gateway_resource.serverless_api.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "opt" {
+  rest_api_id = aws_api_gateway_rest_api.serverless_api.id
+  resource_id = aws_api_gateway_resource.serverless_api.id
+  http_method = aws_api_gateway_method.serverless_api_cors.http_method
+  status_code = 200
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Headers" = true
+  }
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_integration" "serverless_api_cors" {
+  rest_api_id = aws_api_gateway_rest_api.serverless_api.id
+  resource_id = aws_api_gateway_resource.serverless_api.id
+  http_method = aws_api_gateway_method.serverless_api_cors.http_method
+  type        = "MOCK"
+}
+
+resource "aws_api_gateway_integration_response" "serverless_api_cors" {
+  rest_api_id = aws_api_gateway_rest_api.serverless_api.id
+  resource_id = aws_api_gateway_resource.serverless_api.id
+  http_method = aws_api_gateway_method.serverless_api_cors.http_method
+  status_code = 200
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'",
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Requested-With'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST'"
+  }
 }
 
 resource "aws_api_gateway_deployment" "serverless_api_dev" {
