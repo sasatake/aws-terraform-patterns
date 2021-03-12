b95e94e79b12845d5f07c1febe5eb91d64bbcc52
sasatake <t06418as@gmail.com>
2021-03-13 07:55:38 +0900

    add lambda permission update lambda runtime version

--- modules/apigw-lambda/lambda.tf
+++ modules/apigw-lambda/lambda.tf
@@ -1,7 +1,20 @@
 resource "aws_lambda_function" "serverless_api" {
   function_name = "${var.prefix.kebab}-function-serverless-api"
   handler       = "index.handler"
-  runtime       = "nodejs12.x"
+  runtime       = "nodejs14.x"
   role          = aws_iam_role.lambda_execution.arn
   filename      = "${path.module}/files/lambda/package.zip"
+
+  lifecycle {
+    ignore_changes = [filename]
+  }
 }
+
+resource "aws_lambda_permission" "serverless_api" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.serverless_api.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.serverless_api.execution_arn}/*/*/*"
+}
+
