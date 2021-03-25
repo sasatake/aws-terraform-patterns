72890eb2bcc8205fbe6a157d221075287d811517
sasatake <t06418as@gmail.com>
2021-03-25 09:57:31 +0900

    ssm_document のリソース名の修正

--- modules/ecs-ssm/ssm.tf
+++ modules/ecs-ssm/ssm.tf
@@ -8,18 +8,16 @@ resource "aws_ssm_parameter" "ecssrv_task_size" {
   }
 }
 
-resource "aws_ssm_document" "ecssrv-scale-in" {
-  name            = "${var.prefix}-ssmdoc-scale-in"
+resource "aws_ssm_document" "ecssrv_scale_in" {
+  name            = "${var.prefix}-ssmdoc-ecssrv_scale_in"
   document_type   = "Automation"
   document_format = "YAML"
-
-  content = file("${path.module}/templates/ssm/document/scale-in.yaml")
+  content         = file("${path.module}/templates/ssm/document/ecs-scale-in.yaml")
 }
 
-resource "aws_ssm_document" "ecssrv-scale-out" {
-  name            = "${var.prefix}-ssmdoc-scale-out"
+resource "aws_ssm_document" "ecssrv_scale_out" {
+  name            = "${var.prefix}-ssmdoc-ecssrv_scale_out"
   document_type   = "Automation"
   document_format = "YAML"
-
-  content = file("${path.module}/templates/ssm/document/scale-out.yaml")
+  content         = file("${path.module}/templates/ssm/document/ecs-scale-out.yaml")
 }
