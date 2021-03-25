9e1459168d464a50d7fc9b65fa4d73489d2c43cd
sasatake <t06418as@gmail.com>
2021-03-25 10:51:27 +0900

    aws_cloudwatch_event_target 実行対象バージョンの指定および入力パラメータ形式の修正

--- modules/ecs-ssm/cloudwatch.tf
+++ modules/ecs-ssm/cloudwatch.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_event_rule" "ecssrv_scale_in" {
   name                = "${var.prefix}-events-ecssrv_scale_in"
-  schedule_expression = "cron(0 0 ? * MON-FRI *)"
+  schedule_expression = "rate(5 minutes)"
 }
 
 resource "aws_cloudwatch_event_rule" "ecssrv_scale_out" {
@@ -10,26 +10,26 @@ resource "aws_cloudwatch_event_rule" "ecssrv_scale_out" {
 
 resource "aws_cloudwatch_event_target" "ecssrv_scale_in" {
   rule      = aws_cloudwatch_event_rule.ecssrv_scale_in.name
-  arn       = replace(aws_ssm_document.ecssrv_scale_in.arn, "document/", "automation-definition/")
+  arn       = "${replace(aws_ssm_document.ecssrv_scale_in.arn, "document/", "automation-definition/")}:$DEFAULT"
   target_id = aws_ssm_document.ecssrv_scale_in.name
   role_arn  = aws_iam_role.ssm_automation.arn
   input     = <<EOF
     {
-      "ClusterName": "${split("/", aws_ecs_cluster.nginx.id)[1]}",
-      "ServiceName": "${aws_ecs_service.nginx.name}"
+      "ClusterName": ["${split("/", aws_ecs_cluster.nginx.id)[1]}"],
+      "ServiceName": ["${aws_ecs_service.nginx.name}"]
     }
   EOF
 }
 
 resource "aws_cloudwatch_event_target" "ecssrv_scale_out" {
   rule      = aws_cloudwatch_event_rule.ecssrv_scale_out.name
-  arn       = replace(aws_ssm_document.ecssrv_scale_out.arn, "document/", "automation-definition/")
+  arn       = "${replace(aws_ssm_document.ecssrv_scale_out.arn, "document/", "automation-definition/")}:$DEFAULT"
   target_id = aws_ssm_document.ecssrv_scale_out.name
   role_arn  = aws_iam_role.ssm_automation.arn
   input     = <<EOF
     {
-      "ClusterName": "${split("/", aws_ecs_cluster.nginx.id)[1]}",
-      "ServiceName": "${aws_ecs_service.nginx.name}"
+      "ClusterName": ["${split("/", aws_ecs_cluster.nginx.id)[1]}"],
+      "ServiceName": ["${aws_ecs_service.nginx.name}"]
     }
   EOF
 }
